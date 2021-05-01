1bc5f2176300cffff209bd9435c547354c907403
Nihaa21 <nihaghali554@gmail.com>
2021-05-01 19:46:11 +0530

    make it better

--- thatssoveda/settings.py
+++ thatssoveda/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 TEMPLATE_DIR= os.path.join(BASE_DIR,'templates')
 
-STATIC_ROOT= BASE_DIR / "static"
+# STATIC_ROOT= BASE_DIR / "static"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
